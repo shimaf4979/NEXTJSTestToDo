8e4910337e866cffd6f8e571bbb403d06bef33ef
35719024 <pro2.slp.nitech.ac.jp>
2024-10-05 18:58:45 +0900

    dd

--- src/components/NewTaskForm/NewTaskForm.tsx
+++ src/components/NewTaskForm/NewTaskForm.tsx
@@ -57,7 +57,7 @@ const NewTaskForm = () => {
             name='dueDate'
             min='2000-01-01'
             max='2100-12-31'
-            className='p-2mt-2 block w-full rounded-md border-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
+            className='p-2 mt-2 block w-full rounded-md border-gray-300 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600'
           />
         </div>
         <SubmitButton />
