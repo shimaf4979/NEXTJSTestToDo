df291931c7d58b6fde61e8cbc200ac21fd2947fc
35719024 <pro2.slp.nitech.ac.jp>
2024-10-05 18:10:23 +0900

    変わったよん

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,13 +2,25 @@
 
 import React from "react";
 import Link from "next/link";
-const ErrorPage = () => {
+
+interface ErrorPageProps {
+  error: Error;
+  reset: () => void;
+}
+
+const ErrorPage: React.FC<ErrorPageProps> = ({ error, reset }) => {
   return (
     <div className='flex flex-col justify-center items-center h-screen'>
-      <h1 className='text-4xl font-bold'>509090</h1>
-      <p className='text-lg'>Internal Server Error</p>
-      <Link href='/' className='text-blue-500'>
-        Go to Home
+      <h1 className='text-4xl font-bold'>エラーが発生しました</h1>
+      <p className='text-lg mt-4'>{error.message}</p>
+      <button
+        onClick={reset}
+        className='mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'
+      >
+        もう一度試す
+      </button>
+      <Link href='/' className='text-blue-500 mt-4'>
+        ホームに戻る
       </Link>
     </div>
   );
